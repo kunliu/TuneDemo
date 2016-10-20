be34bb305e4dc20d7765ebafc7a80af946d4c467
Kyle <kunliu.cpp@gmail.com>
2016-10-19 19:31:13 -0700

    Add dummy data

---
+++ TuneDemo/app/src/main/java/com/iphrack/tunedemo/CharacterAdapter.java
@@ -0,0 +1,37 @@
+package com.iphrack.tunedemo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by Kyle on 16-10-19.
+ */
+public class CharacterAdapter extends ArrayAdapter{
+
+    public CharacterAdapter(Context context, int resource) {
+        super(context, resource);
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return super.getItem(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        convertView = inflater.inflate(R.layout.character_row, null);
+
+        ((TextView)convertView.findViewById(R.id.name)).setText("asdfasdf");
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+}
--- TuneDemo/app/src/main/java/com/iphrack/tunedemo/MainActivity.java
+++ TuneDemo/app/src/main/java/com/iphrack/tunedemo/MainActivity.java
@@ -2,12 +2,23 @@ package com.iphrack.tunedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CharacterAdapter adapter;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.character_list);
+
+        adapter = new CharacterAdapter(this,0);
+        listView.setAdapter(adapter);
+
+
     }
 }
