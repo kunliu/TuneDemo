5e9ebf4109474b0754100c0d47f73a074702ed6b
Kyle <kunliu.cpp@gmail.com>
2017-01-09 19:16:09 -0800

    Added network call to stage 2 of starter project

--- start/app/src/main/java/com/tune/starter/MainActivity.java
+++ start/app/src/main/java/com/tune/starter/MainActivity.java
@@ -2,9 +2,17 @@ package com.tune.starter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+public class MainActivity extends AppCompatActivity implements Callback{
 
     CharacterAdapter adapter;
     ListView listView;
@@ -18,5 +26,36 @@ public class MainActivity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.character_list);
         listView.setAdapter(adapter);
+
+        loadCharacters();
     }
+
+    private void loadCharacters(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://iphrack.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        TuneService service = retrofit.create(TuneService.class);
+        Call<Object> call = service.listCharacters();
+        call.enqueue(this);
+    }
+
+    @Override
+    public void onResponse(Call call, Response response) {
+        Log.d("ddv",response.toString());
+    }
+
+    @Override
+    public void onFailure(Call call, Throwable t) {
+        Log.d("ddv",t.toString());
+    }
+
+    public interface TuneService{
+        @GET("tune_demo/")
+        Call<Object> listCharacters();
+    }
+
+
+
 }
